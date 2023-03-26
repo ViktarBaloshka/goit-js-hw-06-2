63690e0a22e6d773b11f74a6d2e7a3c667ad5fe8
Viktar Baloshka <vboloshko@gmail.com>
2023-03-26 22:47:45 +0200

    home

--- js/task-01.js
+++ js/task-01.js
@@ -1,5 +1,19 @@
 // Напиши скрипт который:
 
-// Посчитает и выведет в консоль количество категорий в ul#categories, то есть элементов li.item.
-// Для каждого элемента li.item в списке ul#categories, найдет и выведет в консоль текст заголовка элемента (тега <h2>) и количество элементов в категории (всех вложенных в него <li>).
-// Для выполнения этой задачи нужно использовать метод forEach() и свойства навигации по DOM.
+// Посчитает и выведет в консоль количество категорий
+// в ul#categories, то есть элементов li.item.
+// Для каждого элемента li.item в списке ul#categories,
+// найдет и выведет в консоль текст заголовка элемента(тега < h2 >) и
+//  количество элементов в категории(всех вложенных в него < li >).
+// Для выполнения этой задачи нужно использовать
+// метод forEach() и свойства навигации по DOM.
+
+const findElement = document.querySelectorAll(".item").length;
+console.log("Number of categories: " + findElement);
+
+const categoryItemsElement = document
+  .querySelectorAll(".item")
+  .forEach((element) => {
+    console.log("Category: " + element.querySelector("h2").textContent);
+    console.log("Elements: " + element.querySelector("ul").children.length);
+  });
--- js/task-02.js
+++ js/task-02.js
@@ -1,9 +1,11 @@
 // Напиши скрипт, который для каждого элемента массива ingredients:
 
-// Создаст отдельный элемент <li>. Обзательно используй метод document.createElement().
+// Создаст отдельный элемент <li>. Обзательно используй метод
+//document.createElement().
 // Добавит название ингредиента как его текстовое содержимое.
 // Добавит элементу класс item.
-// После чего вставит все <li> за одну операцию в список ul#ingredients.
+// После чего вставит все <li> за одну операцию в список
+// ul#ingredients.
 
 const ingredients = [
   "Potatoes",
@@ -13,3 +15,13 @@ const ingredients = [
   "Herbs",
   "Condiments",
 ];
+const ingredientItemElements = ingredients.map((element) => {
+  const itemsElement = document.createElement("li");
+  itemsElement.textContent = element;
+  itemsElement.classList.add("item");
+  return itemsElement;
+});
+
+const ingredientsListElement = document
+  .querySelector("#ingredients")
+  .append(...ingredientItemElements);
