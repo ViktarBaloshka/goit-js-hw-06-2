83144bc51c8736c491ec58b2a374ca795f69fa76
Viktar Baloshka <vboloshko@gmail.com>
2023-03-27 14:15:05 +0200

    home task

--- js/task-04.js
+++ js/task-04.js
@@ -15,9 +15,9 @@
 
 let counterValue = 0;
 
-const decrementBtn = document.querySelector('button[data-action="decrement"]');
-const incrementBtn = document.querySelector('button[data-action="increment"]');
-const value = document.querySelector("#value");
+const decrementBtn = document.querySelector('[data-action="decrement"]');
+const incrementBtn = document.querySelector('[data-action="increment"]');
+const value = document.querySelector('#value');
 
 const decrementClick = () => {
   counterValue -= 1;
@@ -29,5 +29,5 @@ const incrementClick = () => {
   return (value.textContent = counterValue);
 };
 
-decrementBtn.addEventListener("click", decrementClick);
-incrementBtn.addEventListener("click", incrementClick);
+decrementBtn.addEventListener('click', decrementClick);
+incrementBtn.addEventListener('click', incrementClick);
